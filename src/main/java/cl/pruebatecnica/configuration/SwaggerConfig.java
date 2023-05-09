93b3c2d08e484311801983852e59d0430977593a
GUSTAVO\gusta <gustavoburgos89@outlook.com>
2023-05-09 00:13:08 -0400

    termino

--- src/main/java/cl/pruebatecnica/configuration/SwaggerConfig.java
+++ src/main/java/cl/pruebatecnica/configuration/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.pruebatecnica.controller"))
+                .apis(RequestHandlerSelectors.basePackage("cl.pruebatecnica.controller"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
