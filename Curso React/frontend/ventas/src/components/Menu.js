a5d21aa826b9a1196be498c5c30069cfc1ae66c4
Gustavo <gustavoburgos89@outlook.com>
2023-07-06 12:01:42 -0400

    iniciando proyecto

---
+++ Curso React/frontend/ventas/src/components/Menu.js
@@ -0,0 +1,15 @@
+import React from "react";
+import 'bulma/css/bulma.min.css';
+import { Button } from 'react-bulma-components';
+
+
+const Login = (props) => {
+    return(
+        <div>
+            
+
+        </div>
+    )
+}
+
+export default Login;
