50b3c98fd0efe1e1769351ab8c8b1eb0e7ae1961
SML\gburgos <gustavo.burgos@chilecompra.cl>
2023-07-10 20:21:26 -0400

    validaciones cleinee

--- Curso React/frontend/ventas/src/components/clientes/FormCliente.js
+++ Curso React/frontend/ventas/src/components/clientes/FormCliente.js
@@ -1,95 +1,142 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { ModalContext } from "../../contexts/modal/modalContext";
 
 const FormCliente = () => {
+  const clienteDefault = {
+    nombres: "",
+    apellidos: "",
+    direccion: "",
+    telefono: "",
+    email: "",
+  };
+
+  const { setShowModal } = useContext(ModalContext);
+
+  const [cliente, setCliente] = useState(clienteDefault);
+  const [mensaje, setMensaje] = useState(null);
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if( cliente.nombres === '' && cliente.apellidos === '' && cliente.email === ''){
+        setMensaje("el nombre, apellido y el email son requeridos");
+        return;
+
+    }
+    limpiarForm();
+    cerrarModal();
+    console.log(obtenerClienteAEnviar());
+  };
+
+  const limpiarForm = () => {
+    setMensaje(null);
+    setCliente(clienteDefault);
+  }
+  const handleChange = (e) => {
+    setCliente({
+      ...cliente,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const cerrarModal = () => {
+    limpiarForm();
+    setShowModal(false);
+    
+  }
+  const obtenerClienteAEnviar = () => {
+    let clienteTemp =  {...cliente};
+    if(clienteTemp.direccion === "") delete clienteTemp.direccion;
+    if(clienteTemp.telefono === "") delete clienteTemp.telefono;
+    return clienteTemp
+  }
+
   return (
-    <form>
+    <form onSubmit={handleOnSubmit}>
+        { mensaje ? <div className="notification is-danger">{mensaje}</div> : null}
       <div className="field is-horizontal">
         <div className="field-label is-normal">
-          <label className="label">From</label>
+          <label className="label">Nombre Completo</label>
         </div>
         <div className="field-body">
           <div className="field">
-            <p className="control is-expanded has-icons-left">
-              <input className="input" type="text" placeholder="Name" />
+            <p className="control has-icons-left has-icons-right">
+              <input className="input" type="text" placeholder="Nombres"
+              name="nombres"
+              value={cliente.nombres}
+              onChange={handleChange} />
               <span className="icon is-small is-left">
                 <i className="fas fa-user"></i>
               </span>
             </p>
           </div>
           <div className="field">
-            <p className="control is-expanded has-icons-left has-icons-right">
-              <input
-                className="input is-success"
-                type="email"
-                placeholder="Email"
-                value="alex@smith.com"
-              />
-              <span className="icon is-small is-left">
-                <i className="fas fa-envelope"></i>
-              </span>
-              <span className="icon is-small is-right">
-                <i className="fas fa-check"></i>
-              </span>
-            </p>
+            <input className="input" type="text" placeholder="Apellidos"
+            name="apellidos"
+            value={cliente.apellidos}
+            onChange={handleChange} />
           </div>
         </div>
       </div>
-
       <div className="field is-horizontal">
-        <div className="field-label"></div>
+        <div className="field-label is-normal">
+          <label className="label">Dirección</label>
+        </div>
+        <div className="field-body">
+          <div className="field">
+            <div className="control has-icons-left has-icons-right">
+              <input
+                className="input"
+                type="text"
+                placeholder="Ingrese su dirección"
+                name="direccion"
+                value={cliente.direccion}
+                onChange={handleChange}
+              />
+              <span className="icon is-small is-left">
+                <i className="fa-solid fa-map-location-dot"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="field is-horizontal">
+        <div className="field-label is-normal">
+          <label className="label">Telefono</label>
+        </div>
         <div className="field-body">
           <div className="field is-expanded">
             <div className="field has-addons">
               <p className="control">
-                <a className="button is-static">+44</a>
+                <a className="button is-static">+56 9</a>
               </p>
               <p className="control is-expanded">
                 <input
                   className="input"
                   type="tel"
                   placeholder="Your phone number"
+                  name="telefono"
+                  value={cliente.telefono}
+                  onChange={handleChange}
                 />
               </p>
             </div>
-            <p className="help">Do not enter the first zero</p>
           </div>
         </div>
       </div>
-
       <div className="field is-horizontal">
         <div className="field-label is-normal">
-          <label className="label">Department</label>
+          <label className="label">Email</label>
         </div>
         <div className="field-body">
-          <div className="field is-narrow">
-            <div className="control">
-              <div className="select is-fullwidth">
-                <select>
-                  <option>Business development</option>
-                  <option>Marketing</option>
-                  <option>Sales</option>
-                </select>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="field is-horizontal">
-        <div className="field-label">
-          <label className="label">Already a member?</label>
-        </div>
-        <div className="field-body">
-          <div className="field is-narrow">
-            <div className="control">
-              <label className="radio">
-                <input type="radio" name="member" />
-                Yes
-              </label>
-              <label className="radio">
-                <input type="radio" name="member" />
-                No
-              </label>
+          <div className="field">
+            <div className="control has-icons-left has-icons-right">
+              <input className="input" type="email" placeholder="Email"
+              name="email"
+              value={cliente.email}
+              onChange={handleChange} />
+              <span className="icon is-small is-left">
+                <i className="fa-solid fa-envelope"></i>
+              </span>
             </div>
           </div>
         </div>
@@ -97,45 +144,22 @@ const FormCliente = () => {
 
       <div className="field is-horizontal">
         <div className="field-label is-normal">
-          <label className="label">Subject</label>
+          <label className="label"></label>
         </div>
         <div className="field-body">
-          <div className="field">
-            <div className="control">
-              <input
-                className="input is-danger"
-                type="text"
-                placeholder="e.g. Partnership opportunity"
-              />
-            </div>
-            <p className="help is-danger">This field is required</p>
-          </div>
-        </div>
-      </div>
-
-      <div className="field is-horizontal">
-        <div className="field-label is-normal">
-          <label className="label">Question</label>
-        </div>
-        <div className="field-body">
-          <div className="field">
-            <div className="control">
-              <textarea
-                className="textarea"
-                placeholder="Explain how we can help you"
-              ></textarea>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="field is-horizontal">
-        <div className="field-label"></div>
-        <div className="field-body">
-          <div className="field">
-            <div className="control">
-              <button className="button is-primary">Send message</button>
-            </div>
+          <div className="field is-grouped">
+            <p className="control">
+              <button className="button is-primary mr-1" type="submit">
+                Guardar
+              </button>
+            </p>
+            <p className="control">
+              <button className="button is-light"
+               type="button"
+               onClick={ () => cerrarModal() }>
+                Cancelar
+              </button>
+            </p>
           </div>
         </div>
       </div>
