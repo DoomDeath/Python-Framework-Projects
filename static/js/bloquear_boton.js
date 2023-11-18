ee98ce4dad8e57ddab41638461e28ce41ee3a1dc
Gustavo Burgos <gustavoburgos89@outlook.com>
2023-11-18 14:54:39 -0300

    se agregan valiadciones

--- static/js/bloquear_boton.js
+++ static/js/bloquear_boton.js
@@ -1,13 +1,19 @@
-function enviarFormulario(id) {
+function enviarFormulario(idButton, id) {
     // Deshabilitar el botón al hacer clic
-    var boton = document.getElementById("botonEnviar");
+    var boton = document.getElementById(idButton);
     boton.disabled = true;
+
+    // Cambiar el color del botón deshabilitado (por ejemplo, a gris)
+    boton.style.backgroundColor = '#CCCCCC'; // Puedes ajustar el color según tus necesidades
+
+    // Cambiar el texto del botón a "Cargando..."
+    boton.innerHTML = 'Cargando...';
 
     // Enviar el formulario
     var formulario = document.getElementById(id);
     formulario.submit();
 
-    console.log("SE PRESIONO")
+    console.log("SE PRESIONÓ");
 
     // Opcionalmente, puedes mostrar un mensaje de espera o realizar otras acciones necesarias
 }
