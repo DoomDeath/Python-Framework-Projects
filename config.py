41b470be65bf44db9f460076ec035b12d6266874
SML\gburgos <gustavo.burgos@chilecompra.cl>
2023-11-23 22:06:07 -0300

    se actualiza token

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ SECRET_KEY = '12345678'  # Clave secreta para proteger las sesiones
 
 GITHUB_USERNAME = 'DoomDeath'
 GITHUB_REPO = 'imagenes'
-GITHUB_TOKEN = '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'
+GITHUB_TOKEN = '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'
