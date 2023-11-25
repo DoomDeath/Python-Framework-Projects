9abc2941226e865211aa53b525b0991353baf301
Gustavo <gustavoburgos89@outlook.com>
2023-11-24 21:12:25 -0300

    token actualizado

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ SECRET_KEY = '12345678'  # Clave secreta para proteger las sesiones
 
 GITHUB_USERNAME = 'DoomDeath'
 GITHUB_REPO = 'imagenes'
-GITHUB_TOKEN = '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'
+GITHUB_TOKEN = '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'
